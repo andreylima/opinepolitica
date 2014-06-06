17149813926f2c73cba35c0f93a83814d099572d
Andrey Lima Ramos <andreylrgv@hotmail.com>
2014-06-06 18:02:24 -0300

    05062014
    frase destaque

--- politico/page-mobile.php
+++ politico/page-mobile.php
@@ -6,6 +6,14 @@ get_header('mobile');
 ?>
 
 <?php wp_nav_menu( array( 'theme_location'=>'principal', 'container_id' => 'menu-body' ) ); ?>
+<div class="destaque">
+<div class="wrap-destaque">
+	<h1>OPINE SOBRE OS PROJETOS DE LEI</h1> 
+	<h1>PROPOSTOS EM GOVERNADOR VALADARES</h1>
+</div>
+</div>
+
+
 
 <?php get_footer() ?>
 
