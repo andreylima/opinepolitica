e5015351b37633c3434ee632a9a6f9ec4a1dbb67
andrey <andreylrgv@hotmail.com>
2015-01-12 00:01:28 -0200

    12012015

--- politico/page-registrar-denuncia.php
+++ politico/page-registrar-denuncia.php
@@ -8,13 +8,20 @@ get_header();
 <div id="page-wrapper-2" class="page-denuncia">
 <div class="rd-city hide">Governador Valadares</div>
 <div class="rd-state hide">MG</div>
+<form action="" method="post" id="denuncia_form" novalidate="novalidate">
 <input id="lat" name="denuncia[latitude]" type="hidden" value="">
 <input id="addressField" name="denuncia[endereco]" type="hidden" value="">
 <input id="lng" name="denuncia[longitude]" type="hidden" value="">
 
 <h3>1) Endereço</h3>
-<h4>Informe o endereço do local denunciado</h4>
-
+<h4>Informe o endereço do local denunciado no campo de texto ou arraste o marcador até o local correto.</h4>
+<?php if ( !is_user_logged_in() ) { 
+?>
+<div class="registered-users-required">
+	A denúncia só pode ser feita por usuários cadastrados.</br> Faça Login e tente novamente.
+</div>
+<?php
+} ?>
 <input type="text" placeholder="Buscar Endereço" class="search-adress">
 <div id="mapagv-register"></div>
 <div class="active-adress"></div>
@@ -25,7 +32,7 @@ get_header();
 
 <div class="wrap-line">
 <label for="denuncia-title" class="label-denuncia-title">Título da Denúncia</label>
-<input type="text" class="denuncia-title" placeholder="Ex: Buraco, Descaso, Lixo ....">
+<input type="text" class="denuncia-title" name="denuncia_title" placeholder="Ex: Buraco, Descaso, Lixo ....">
 </div>
 
 <div class="descricao-box">
@@ -40,16 +47,36 @@ get_header();
 <div class="wrap-line">
 <div class="youtube-upload-box float-left">
 <label for="youtube-video" >Link do vídeo do Youtube:</label>  <input type="text" class="youtube-video" placeholder=" Suba seu vídeo no youtube e cole o link aqui..."> 
+<div class="tip-link">O vídeo deve mostrar o problema apontado na denúncia.</div>
+</div>
+<div class="option-video "> 
+<div class="debate-filma-box">
+<input type="radio" name="debate-video" class="debate-video" value="yes">
+<label for="debate-video" class="debate-video-label">Gostaria que o DEBATEGV fizesse o vídeo da denúncia.</label>
+<div class="tip-link">O DEBATEGV irá até o local da denúncia fazer a gravação do vídeo.</div>
+</div>
+<div class="user-personagem-box">
+<input type="checkbox" name="user-personagem" class="user-personagem" value="yes">
+<label for="user-personagem">Gostaria de ser um personagem do vídeo.</label>
+<div class="tip-link">O DEBATEGV entrará em contato por e-mail para combinar o melhor horário para a gravação.</div>
+</div>
 </div>
 
 </div>
 
-
-
+<?php if ( !is_user_logged_in() ) { 
+?>
+<div class="registered-users-required">
+	A denúncia só pode ser feita por usuários cadastrados.</br> Faça Login e tente novamente.
 </div>
+<?php
+}
+else
+{?>
+  <input type="submit" name="registrar-denuncia" value="REGISTRAR DENÚNCIA" id="registrar-denuncia" >
 
-
-
+<?php } ?>
+</form>
 
 
 <?php 
